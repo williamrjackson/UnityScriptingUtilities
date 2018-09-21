4a7ec1f17dfc55d38ab843ed5cb38d6284374a86
Bill Jackson <bjackson@insightfulengine.com>
2018-09-21 13:22:58 -0400

    New trail for each "pen lift" in path demo

--- Assets/Example/PathFollowTest.cs
+++ Assets/Example/PathFollowTest.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PathFollowTest : MonoBehaviour {
     public float speed = 5;
     public Wrj.BezierPath[] paths;
-
+    public TrailRenderer trail;
     private int pathIndex = 0;
 	// Use this for initialization
 	void Start () {
@@ -15,6 +15,11 @@ public class PathFollowTest : MonoBehaviour {
     {
         if (pathIndex < paths.Length)
         {
+            trail.transform.parent = null;
+            Vector3 look = Vector3.zero;
+            transform.position = paths[pathIndex].GetPointOnCurve(0, ref look);
+            trail = Instantiate(trail.gameObject, transform).GetComponent<TrailRenderer>();
+            trail.transform.localPosition = Vector3.zero;
             Wrj.Utils.MapToCurve.Ease.MoveAlongPath(transform, paths[pathIndex], speed * paths[pathIndex].GetCurveLength(), onDone: RunPath);
             pathIndex++;
         }
