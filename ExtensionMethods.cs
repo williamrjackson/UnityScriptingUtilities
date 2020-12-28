67f397558d0973c42f2ea222c2e5afca2c5415f8
Bill Jackson <williamrjackson@gmail.com>
2020-12-28 14:07:27 -0500

    Documentation touchups in ExtensionMethods

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -5,86 +5,126 @@ using UnityEngine.UI;
 
 public static class ExtensionMethods
 {
+    /// <summary>
     /// Returns a component of Type T by finding the existing one, or by instantiating one if not found.
+    /// </summary>
     public static T EnsureComponent<T>(this GameObject go) where T : Component
     {
         return  Wrj.Utils.EnsureComponent<T>(go);
     }
+    /// <summary>
     /// Returns a component of Type T by finding the existing one, or by instantiating one if not found.
+    /// </summary>    
     public static T EnsureComponent<T>(this Transform tform) where T : Component
     {
         return tform.gameObject.EnsureComponent<T>();
     }
+    /// <summary>
     /// Returns a component of Type T by finding the existing one, or by instantiating one if not found.
+    /// </summary>
     public static T EnsureComponent<T>(this Component comp) where T : Component
     {
         return comp.gameObject.EnsureComponent<T>();
     }
-    
-    /// Runs an operation on every child of the game object recursively.
+
+    /// <summary>
+    /// <para>Runs an operation on every child of the game object recursively.</para>
     ///
     /// Argument is a method that takes a GameObject.
+    /// </summary>
     public static void PerChild(this GameObject go, Wrj.Utils.GameObjectAffector goa)
     {
         Wrj.Utils.AffectGORecursively(go, goa, true);
     }
 
+    /// <summary>
     /// Enable or disable GameObject in hierarchy.
+    /// </summary>
     public static void ToggleActive(this GameObject go)
     {
         go.SetActive(!go.activeInHierarchy);
     }
+    /// <summary>
     /// Enable or disable GameObject in hierarchy.
+    /// </summary>
     public static void ToggleActive(this Transform tForm)
     {
         tForm.gameObject.SetActive(!tForm.gameObject.activeInHierarchy);
     }
 
+    /// <summary>
     /// Returns a Vector3 with an axis forced as specified
+    /// </summary>
     public static Vector3 With(this Vector3 orig, float? x = null, float? y = null, float? z = null)
     {
         return new Vector3(x ?? orig.x, y ?? orig.y, z ?? orig.z);
     }
-
+    /// <summary>
     /// Returns the transforms position as moved in a direction relative to itself
+    /// </summary>
     public static Vector3 PosInDir(this Transform tform, float forward = 0f, float right = 0f, float up = 0f)
     {
         return tform.position + tform.forward * forward + tform.right * right + tform.up * up;
     }
-    
+
+    /// <summary>
     /// Returns the transforms position as moved in a direction relative to the world
+    /// </summary>
     public static Vector3 PosInWorldDir(this Transform tform, float forward = 0f, float right = 0f, float up = 0f)
     {
         return tform.position + Vector3.forward * forward + Vector3.right * right + Vector3.up * up;
     }
 
+    /// <summary>
     /// Returns the transforms local position as moved in a direction relative to itself
+    /// </summary>
     public static Vector3 LocalPosInDir(this Transform tform, float forward = 0f, float right = 0f, float up = 0f)
     {
         return tform.localPosition + tform.forward * forward + tform.right * right + tform.up * up;
     }
 
+    /// <summary>
     /// Returns the transforms local position as moved in a direction relative to the world
+    /// </summary>
     public static Vector3 LocalPosInWorldDir(this Transform tform, float forward = 0f, float right = 0f, float up = 0f)
     {
         return tform.localPosition + Vector3.forward * forward + Vector3.right * right + Vector3.up * up;
     }
 
+    /// <summary>
     /// Returns a Vector3 using x, y and 0
+    /// </summary>
     public static Vector3 ToVector3(this Vector2 v2)
     {
         return new Vector3(v2.x, v2.y, 0);
     }
+    /// <summary>
     /// Returns a Vector2 using x and y of the Vector3
+    /// </summary>
     public static Vector2 ToVector2(this Vector3 v3)
     {
         return new Vector2(v3.x, v3.y);
     }
 
-    
+    /// <summary>
+    /// Returns the angle represtented by a 2D direction
+    /// </summary>
+    public static float Angle(this Vector2 v2)
+    {
+        if (v2.x < 0)
+        {
+            return 360 - (Mathf.Atan2(v2.x, v2.y) * Mathf.Rad2Deg * -1);
+        }
+        else
+        {
+            return Mathf.Atan2(v2.x, v2.y) * Mathf.Rad2Deg;
+        }
+    }
 
+    /// <summary>
     /// Returns a random element of a list.
-    public static T GetRandom<T>(this System.Collections.Generic.List<T> list)
+    /// </summary>
+    public static T GetRandom<T>(this List<T> list)
     {
         if (list.Count > 0)
         {
@@ -92,7 +132,9 @@ public static class ExtensionMethods
         }
         return default(T);
     }
+    /// <summary>
     /// Returns a random element of an array.
+    /// </summary>
     public static T GetRandom<T>(this T[] ar)
     {
         if (ar.Length > 0)
@@ -102,22 +144,30 @@ public static class ExtensionMethods
         return default(T);
     }
 
+    /// <summary>
     /// Returns conversion to Unity Units/Meters from Feet
+    /// </summary>
     public static float FeetToUnits(this float feet)
     {
         return Wrj.Utils.FromFeet(feet);
     }
+    /// <summary>
     /// Returns conversion to Feet from Unity Units/Meters
+    /// </summary>
     public static float UnitsToFeet(this float units)
     {
         return Wrj.Utils.ToFeet(units);
     }
+    /// <summary>
     /// Returns conversion to Inches from Unity Units/Meters
+    /// </summary>
     public static float InchesToUnits(this float inches)
     {
         return Wrj.Utils.FromInches(inches);
     }
+    /// <summary>
     /// Returns conversion to Unity Units/Meters from Inches
+    /// </summary>
     public static float UnitsToInches(this float units)
     {
         return Wrj.Utils.ToInches(units);
@@ -151,7 +201,7 @@ public static class ExtensionMethods
 
     private static bool HasRulebreakingIndefiniteArticle(string word)
     {
-        string list = " a f h l m n r s u x 8 11 18 80 81 82 83 84 85 86 87 88 89 honest honesty hour heir honour honourable honor honorable herb use union university unit user unity universe uniform usage utility urine uranium unison euphoria utopia unanimity uterus euthanasia ewe ufo unicorn urea urethra euphemism eugenics usurper usability eunuch uni eucalyptus usury eulogy ubiquity universalism urinal universal ewer euro utensil ufology uniformitarianism upsilon ukulele urinalysis usurer ureter uridine ute eugenist eutectic eukaryote ufologist ululation usufruct eustasy unary uvula urus eucatastrophe uraeus ouabain one using ucalegon oncer usanian usufruction eusebius usar usufructuary amazigh usuress euouae ukase euclidianness uke uke uke ukie ureteroureterostomy usurping eustress unakas eudaemon ukrainian unidirectionality utahn unite uranism uranist eudemonia euth ute uranophobia euphoriant uvular ouija uropygium eugarie eugenesis uw iatmul eutripsia uey eugeny euglena ufo unigeniture univalence univalent utile utilitarian ubac eulachon unique usonian oaxaca uniquity eureka onesie universalism uberty uni ubication utonian ubicity euboean uniate euro utopographer esclop euro-american eumenides eucharist univocal euchologion euchre eunoia unix ";
+        string list = " f h l m n r s u x 8 11 18 80 81 82 83 84 85 86 87 88 89 honest honesty hour heir honour honourable honor honorable herb use union university unit user unity universe uniform usage utility urine uranium unison euphoria utopia unanimity uterus euthanasia ewe ufo unicorn urea urethra euphemism eugenics usurper usability eunuch uni eucalyptus usury eulogy ubiquity universalism urinal universal ewer euro utensil ufology uniformitarianism upsilon ukulele urinalysis usurer ureter uridine ute eugenist eutectic eukaryote ufologist ululation usufruct eustasy unary uvula urus eucatastrophe uraeus ouabain one using ucalegon oncer usanian usufruction eusebius usar usufructuary amazigh usuress euouae ukase euclidianness uke uke uke ukie ureteroureterostomy usurping eustress unakas eudaemon ukrainian unidirectionality utahn unite uranism uranist eudemonia euth ute uranophobia euphoriant uvular ouija uropygium eugarie eugenesis uw iatmul eutripsia uey eugeny euglena ufo unigeniture univalence univalent utile utilitarian ubac eulachon unique usonian oaxaca uniquity eureka onesie universalism uberty uni ubication utonian ubicity euboean uniate euro utopographer esclop euro-american eumenides eucharist univocal euchologion euchre eunoia unix ";
         return list.IndexOf(" " + word.ToLower()+ " ") >= 0;
     }
 
@@ -164,15 +214,15 @@ public static class ExtensionMethods
     }
 
     /// <summary>
-    /// Appends "s" or "es" based on common rules.
+    /// <para>Appends "s" or "es" based on common rules. Uses dictionary of common outliers.</para>
+    /// "Puma" -> "Pumas"; "Fox" -> "Foxes"; "Index" -> "Indices"
     /// </summary>
-    /// <returns>"Puma" -> "Pumas" or "Fox" -> "Foxes"</returns>
     public static string Pluralize(this string word, bool capitalize = false)
     {
-        string checkedWord = CheckForIrregularPlural(word);
-        if (checkedWord != null)
+        string foundIrregular = CheckForIrregularPlural(word);
+        if (foundIrregular != null)
         {
-            return Capitalize(checkedWord, capitalize);
+            return Capitalize(foundIrregular, capitalize);
         }
         char[] chars = word.ToCharArray();
 
@@ -209,147 +259,196 @@ public static class ExtensionMethods
     }
     
     // Shorthand transform manipulation extensions, using MapToCurve
-    
-    /// Move, rotate and scale the transform to the position of another over time
-    ///
+    /// <summary>
+    /// <para>Move, rotate and scale the transform to the position of another over time.</para>
+    /// 
     /// Strongly recommended that the target transform shares the parent of this transform.
+    /// </summary>
+
     public static void SnapToSibling(this Transform tForm, Transform to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.MatchSibling(tForm, to, duration, false, 0, 0, 0, false, null);
     }
-    /// Move, rotate and scale the transform to the position of another over time
-    ///
+    /// <summary>
+    /// <para>Move, rotate and scale the transform to the position of another over time.</para>
+    /// 
     /// Strongly recommended that the target transform shares the parent of this transform.
+    /// </summary>
     public static void SnapToSibling(this GameObject go, Transform to, float duration)
     {
         go.transform.SnapToSibling(to, duration);
     }
-    /// Move, rotate and scale the transform to the position of another over time
+    /// <summary>
+    /// <para>Move, rotate and scale the transform to the position of another over time.</para>
     ///
     /// Strongly recommended that the target transform shares the parent of this transform.
+    /// </summary>
     public static void EaseSnapToSibling(this Transform tForm, Transform to, float duration)
     {
         Wrj.Utils.MapToCurve.Ease.MatchSibling(tForm, to, duration, false, 0, 0, 0, false, null);
     }
-    /// Move, rotate and scale the transform to the position of another over time
+    /// <summary>
+    /// <para>Move, rotate and scale the transform to the position of another over time.</para>
     ///
     /// Strongly recommended that the target transform shares the parent of this transform.
+    /// </summary>
     public static void EaseSnapToSibling(this GameObject go, Transform to, float duration)
     {
         go.transform.EaseSnapToSibling(to, duration);
     }
-
+    /// <summary>
     /// Move transform in local space over time
+    /// </summary>
     public static void Move(this Transform tForm, Vector3 to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.MoveWorld(tForm, to, duration, false, 0, 0, 0, false, false, null);
     }
+    /// <summary>
     /// Move transform in local space over time
+    /// </summary>
     public static void Move(this GameObject go, Vector3 to, float duration)
     {
         go.transform.Move(to, duration);
     }
+    /// <summary>
     /// Move transform in local space over time
+    /// </summary>
     public static void EaseMove(this Transform tForm, Vector3 to, float duration)
     {
         Wrj.Utils.MapToCurve.Ease.MoveWorld(tForm, to, duration, false, 0, 0, 0, false, false, null);
     }
+    /// <summary>
     /// Move transform in local space over time
+    /// </summary>
     public static void EaseMove(this GameObject go, Vector3 to, float duration)
     {
         go.transform.EaseMove(to, duration);
     }
-    
+    /// <summary>
     /// Rotate transform over time
+    /// </summary>
     public static void LinearRotate(this Transform tForm, Vector3 to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.Rotate(tForm, to, duration, false, 0, 0, 0, false, true, false, null);
     }
+    /// <summary>
     /// Rotate transform over time
+    /// </summary>
     public static void Rotate(this GameObject go, Vector3 to, float duration)
     {
         go.transform.LinearRotate(to, duration);
     }
+    /// <summary>
     /// Rotate transform over time
+    /// </summary>
     public static void EaseRotate(this Transform tForm, Vector3 to, float duration)
     {
         Wrj.Utils.MapToCurve.Ease.Rotate(tForm, to, duration, false, 0, 0, 0, false, true, false, null);
     }
+    /// <summary>
     /// Rotate transform over time
+    /// </summary>
     public static void EaseRotate(this GameObject go, Vector3 to, float duration)
     {
         go.transform.EaseRotate(to, duration);
     }
-    
+
+    /// <summary>
     /// Change transforms scale over time
+    /// </summary>
     public static void Scale(this Transform tForm, Vector3 to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.Scale(tForm, to, duration, false, 0, 0, 0, false, null);
     }
+    /// <summary>
     /// Change transforms scale over time using a multiplier
+    /// </summary>
     public static void Scale(this Transform tForm, float to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.Scale(tForm, tForm.localScale * to, duration, false, 0, 0, 0, false, null);
     }
+    /// <summary>
     /// Change transforms scale over time
+    /// </summary>
     public static void Scale(this GameObject go, Vector3 to, float duration)
     {
         go.transform.Scale(to, duration);
     }
+    /// <summary>
     /// Change transforms scale over time using a multiplier
+    /// </summary>
     public static void Scale(this GameObject go, float to, float duration)
     {
         go.transform.Scale(to, duration);
     }
+    /// <summary>
     /// Change transforms scale over time
+    /// </summary>
     public static void EaseScale(this Transform tForm, Vector3 to, float duration)
     {
         Wrj.Utils.MapToCurve.Ease.Scale(tForm, to, duration, false, 0, 0, 0, false, null);
     }
+    /// <summary>
     /// Change transforms scale over time using a multiplier
+    /// </summary>
     public static void EaseScale(this Transform tForm, float to, float duration)
     {
         Wrj.Utils.MapToCurve.Ease.Scale(tForm, tForm.localScale * to, duration, false, 0, 0, 0, false, null);
     }
+    /// <summary>
     /// Change transforms scale over time
+    /// </summary>
     public static void EaseScale(this GameObject go, Vector3 to, float duration)
     {
         go.transform.EaseScale(to, duration);
     }
+    /// <summary>
     /// Change transforms scale over time using a multiplier
+    /// </summary>
     public static void EaseScale(this GameObject go, float to, float duration)
     {
         go.transform.EaseScale(to, duration);
     }
-    
+
+    /// <summary>
     /// Change Color Over Time
+    /// </summary>
     public static void Color(this Transform tForm, Color to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.ChangeColor(tForm, to, duration, false, 0, 0, 0, false, null);
     }
+    /// <summary>
     /// Change Color Over Time
+    /// </summary>
     public static void Color(this GameObject go, Color to, float duration)
     {
         go.transform.Color(to, duration);
     }
-
+    /// <summary>
     /// Change transparency over time
+    /// </summary>
     public static void Alpha(this Transform tForm, float to, float duration)
     {
         Wrj.Utils.MapToCurve.Linear.FadeAlpha(tForm, to, duration, false, 0, 0, 0, false, null);
     }
+    /// <summary>
     /// Change transparency over time
+    /// </summary>
     public static void Alpha(this GameObject go, float to, float duration)
     {
         go.transform.Alpha(to, duration);
     }
 
+    /// <summary>
     /// Remap Float value from one range to another.
+    /// </summary>
     public static float Remap(this float value, float sourceMin, float sourceMax, float destMin, float destMax )
     {
         return Wrj.Utils.Remap(value, sourceMin, sourceMax, destMin, destMax);
     }
+    /// <summary>
     /// Remap Float value from one range to another.
+    /// </summary>
     public static float Remap(this int value, float sourceMin, float sourceMax, float destMin, float destMax )
     {
         return Wrj.Utils.Remap(value, sourceMin, sourceMax, destMin, destMax);
