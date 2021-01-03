d3767241665ab8178425fe7608e8d5b22196689e
Bill Jackson <williamrjackson@gmail.com>
2021-01-03 13:01:32 -0500

    SmoothFollow component

---
+++ SmoothFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Wrj
+{
+	public class SmoothFollow : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform followTarget;
+		[SerializeField]
+		private float smoothTime = 0.3F;
+		[SerializeField]
+		[Range(0f, 1f)]
+		private float distanceThreshold = .1f;
+
+		private Vector3 offset;
+		private Vector3 velocity = Vector3.zero;
+
+		void Start()
+		{
+			offset = transform.position - followTarget.position;
+		}
+
+		void LateUpdate()
+		{
+			Vector3 targetPosition = followTarget.position + offset;
+			if (Vector3.Distance(transform.position, targetPosition) > distanceThreshold)
+			{
+				transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+			}
+		}
+	}
+}
